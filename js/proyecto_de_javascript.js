f76e039adb997f567714bac20d965f93b39f9b78
SaymondHA <saymax21elcrack@gmail.com>
2022-05-20 19:34:52 -0500

    proyect

--- js/proyecto_de_javascript.js
+++ js/proyecto_de_javascript.js
@@ -3,11 +3,13 @@ var submit_button = document.getElementById("send")
 submit_button.onclick = function(event){
   event.preventDefault();
   var email = document.getElementById("email_input").value;
+  var name = document.getElementById("name_input").value;
+  var text= document.getElementById("message").value;
   Swal.fire({
-    title: '<strong>HTML <u>example</u></strong>',
+    title: '<strong> your information is</strong>',
     icon: 'info',
     html:
-      'El email que has ingresado es: '+email+'el valor del texto es:',
+      'inserted email value is : '+email+' inserted name value is '+name+' inserted information is '+text,
     showCloseButton: true,
     showCancelButton: true,
     focusConfirm: false,
